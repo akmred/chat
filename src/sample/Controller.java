d87507132dafa7346c51b8281770315c0d599dbb
Anton Korzhov <akmred@gmail.com>
2019-12-01 21:47:14 +0300

    1. Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна. Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели. Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter. При «отсылке» сообщение перекидывается из нижнего поля в центральное.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,72 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.FocusModel;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import jdk.nashorn.internal.runtime.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    @FXML public ListView idContacts;
+    @FXML public TextField idSendText;
+    @FXML public TextArea idTextMessage;
+
+    public void setIdTextMessage(TextArea idTextMessage) {
+        this.idTextMessage = idTextMessage;
+    }
+
+    private List<String> startList= new ArrayList<>();
+    private ObservableList oList = FXCollections.observableArrayList();
+
+
+    public ListView getIdContacts() {
+        return idContacts;
+    }
+
+    public void setIdContacts() {
+        this.idContacts = idContacts;
+
+        this.startList.add("Lena");
+        this.startList.add("Pasha");
+        this.startList.add("Katya");
+
+        oList.setAll(startList);
+        idContacts.setItems(oList);
+
+    }
+
+    @FXML
+    void initialize(){
+        setIdContacts();
+    }
+
+
+    public void OnCkicedList(MouseEvent mouseEvent) {
+
+        this.idTextMessage.clear();
+
+    }
+
+    public void onActionMouseSend(ActionEvent actionEvent) {
+
+        if (!idTextMessage.getText().isEmpty()) {
+            this.idTextMessage.appendText("\n");
+        }
+
+
+        this.idTextMessage.appendText("("+ this.idContacts.getId() + ")" + idSendText.getText());
+        idSendText.clear();
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,11 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("chat anton korzhov");
+        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
